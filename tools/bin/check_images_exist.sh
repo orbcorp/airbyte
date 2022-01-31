cbe15909a1673857c1dd90d94b32a6c6c66b95ff
Harshith Mullapudi <harshithmullapudi@gmail.com>
2022-01-31 17:51:28 +0530

    fix: check for only airbyte/ images and leave external images

--- tools/bin/check_images_exist.sh
+++ tools/bin/check_images_exist.sh
@@ -28,7 +28,8 @@ checkNormalizationImages() {
 checkConnectorImages() {
   echo "Checking connector images exist..."
 
-  CONNECTOR_DEFINITIONS=$(grep "dockerRepository" -h -A1 airbyte-config/init/src/main/resources/seed/*.yaml | grep -v -- "^--$" | tr -d ' ')
+  # Added `: airbyte/` since we have added external images also and we don't' want to check those images
+  CONNECTOR_DEFINITIONS=$(grep "dockerRepository: airbyte/" -h -A1 airbyte-config/init/src/main/resources/seed/*.yaml | grep -v -- "^--$" | tr -d ' ')
   [ -z "CONNECTOR_DEFINITIONS" ] && echo "ERROR: Could not find any connector definition." && exit 1
 
   while IFS=":" read -r _ REPO; do
